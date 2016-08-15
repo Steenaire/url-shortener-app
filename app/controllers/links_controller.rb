0db80c759f7568552a2a0f9c2b617c38c893cee6
Steen Clarke <cmclarkey@gmail.com>
2016-08-14 21:00:23 -0700

    Add links where user_id = current_user.id in index

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,7 +3,26 @@ class LinksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @links = Link.find_by(user_id: current_user.id)
+    @links = Link.where(user_id: current_user.id)
+  end
+
+  def new
+  end
+
+  def create
+    link = Link.new(
+      user_id: current_user.id,
+      slug: params[:slug],
+      target_url: params[:target_url]
+      )
+
+    if link.save
+      flash[:success] = "URL Shortened!"
+      redirect_to "/links"
+    else
+      flash[:danger] = "URL Not Shortened"
+      render 'new'
+    end
   end
 
 end
